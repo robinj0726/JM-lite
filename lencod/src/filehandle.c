b07f257f54a9d132c6fcd0d285f2bcd3dc8f52d7
robinj730 <88188467+robinj730@users.noreply.github.com>
2023-12-11 22:33:35 +0800

    print out the error messages

--- lencod/src/filehandle.c
+++ lencod/src/filehandle.c
@@ -1,3 +1,9 @@
+#include "global.h"
+
 void error(char *text, int code)
 {
+  fprintf(stderr, "%s\n", text);
+  // flush_dpb(p_Enc->p_Vid->p_Dpb_layer[0], &p_Enc->p_Inp->output);
+  // flush_dpb(p_Enc->p_Vid->p_Dpb_layer[1], &p_Enc->p_Inp->output);
+  exit(code);
 }

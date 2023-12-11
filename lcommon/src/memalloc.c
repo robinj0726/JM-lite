ab70f42a1901a755248b99400d5bd8092e5bd5c1
robinj730 <88188467+robinj730@users.noreply.github.com>
2023-12-11 22:43:25 +0800

    no_mem_exit works

--- lcommon/src/memalloc.c
+++ lcommon/src/memalloc.c
@@ -1,7 +1,8 @@
+#include "global.h"
 #include "memalloc.h"
 
 void no_mem_exit(char *where)
 {
-  //  snprintf(errortext, ET_SIZE, "Could not allocate memory: %s",where);
-  //  error (errortext, 100);
+   snprintf(errortext, ET_SIZE, "Could not allocate memory: %s",where);
+   error (errortext, 100);
 }
